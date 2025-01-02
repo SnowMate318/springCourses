e5f20638f14f0ae0decd2532e004b534a793b47c
taehyun <xogus5070318@gmail.com>
2025-01-02 22:58:59 +0900

    docs(스프링_개념.md, AppConpig, MemberApp, OrderApp): 스프링 적용 및 스프링 개념 정리
    기존 AppConpig를 스프링 컨테이너로 변경 및 적용된 스프링 개념을 정리하엿습니다.
    
    BREAKING CHANGE: None

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,24 +9,37 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 // 의존관계 주입 -> dependancy injection
 public class AppConfig { // 실제 동작에 필요한 구현객체
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy() {
-        return new RateDiscountPolicy(); // 
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy(); //
     }
 
 }
+
+//@Configuration
+//스프링에서의 설정 정보에는 위 어노테이션을 붙여줌
+
+//@Bean
+//스프링 컨테이너에 등록
--- core/src/main/java/hello/core/member/MemberApp.java
+++ core/src/main/java/hello/core/member/MemberApp.java
@@ -1,12 +1,23 @@
 package hello.core.member;
 
 import hello.core.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService(); // 여긴 memberServiceImpl이 들어가있겠지
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService(); // 여긴 memberServiceImpl이 들어가있겠지
 
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //@Bean 이라된걸 다 관리해줌
+        //AppConfig에 있는 설정 정보를가지고 @Bean을 스프링 컨테이너에서 다 관리해줌
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+        // "memberService" -> 네임 파라미터: 함수 이름 기반으로 검색
+        
+        
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
